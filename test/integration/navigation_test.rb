056f6465112e3de269ce53bd0b41515a966374c3
Jeff Siegel <jdsiegel@gmail.com>
2013-01-15 00:05:48 -0700

    basic integration test

--- test/integration/navigation_test.rb
+++ test/integration/navigation_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class NavigationTest < ActiveSupport::IntegrationCase
-  test 'home index page renders' do
-    #visit '/'
+  test 'payment index page renders a beanstream form' do
+    visit payment_path
+    assert page.has_selector?("form#sample_form")
   end
 end
